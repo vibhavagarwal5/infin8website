0274bcc83be4b226e79c7cb9fd44d15df0403d94
Vishesh Ruparelia <visheshruparelia18@gmail.com>
2018-01-14 12:23:00 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
     # since you don't have to restart the web server when you make code changes.
+  config.action_mailer.default_url_options = { :host => 'infin8.org' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
