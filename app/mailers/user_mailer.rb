655aaf46986fa4f1fee65fb176034cc6f3227250
Vishesh <visheshruparelia18@gmail.com>
2018-01-14 23:47:39 +0530

    added cc in query

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
     @name=name
     @mail=mail
     @query=query
-    mail(to: 'infin8-2018@iiitb.org', subject: 'Query from ' + @name)
+    mail(to: 'infin8-2018@iiitb.org', subject: 'Query from ' + @name, cc: 'vishesh.ruparelia@iiitb.org')
   end
   def bulk_mail(user, subject, body)
       @user=user
